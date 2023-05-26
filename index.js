77bf6e2510cd8ae8719edce7200b2c81610db787
Ricardo Silva <ric.silva15@gmail.com>
2023-05-26 05:02:24 -0600

    Logica de javascript terminada sin estilos CSS

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let notas = [10,8,9,7,7]
+let notas = [9,4,10,7,8]
 
 function calificaciones(){
     let elementoListaNotas = document.getElementById("listaNotas")
@@ -11,16 +11,13 @@ function calificaciones(){
     let mostrandoCalificaciones = qualy
     itemLista.innerText = mostrandoCalificaciones
         console.log(qualy)
-    
-    }
+     }
 }
 
 function promedio(){
     let nota = 0
     //let promedios = document.getElementById("Promedio").value
    //document.getElementById("Promedio").value = promedioRicardo
-    
-    
     
     for (i = 0; i < notas.length; i++){
         
@@ -32,7 +29,38 @@ function promedio(){
     document.getElementById("Promedio").value =  promedioRicardo
     
     console.log(promedioRicardo)
-
-    
 }
 
+
+
+function notaMasAlta(){
+    let notaAlta = 0;
+    let i = 0;
+
+    while (i < 5){
+        if (notas[i] > notaAlta){
+            notaAlta = notas[i]
+        }
+       i++
+        
+    }
+
+    console.log(notaAlta)
+    document.getElementById("notaAlta").value = notaAlta
+}
+
+function reprobaste(){
+    let materiaReprobada = "no";
+    let i = 0
+
+    do{
+        if(notas[i] < 6){
+            materiaReprobada = "si"
+            console.log(i)
+            break
+        }i++
+        
+    }while(i < 5) 
+    
+    console.log("Reprobaste la materia con" + materiaReprobada)
+}
